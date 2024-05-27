022ec20b7953dd74cd15c73667468b8915176f16
aidendk <aduceykline@gmail.com>
2024-05-27 18:27:33 -0500

    update task

--- src/app/[id].tsx
+++ src/app/[id].tsx
@@ -1,13 +1,36 @@
 import { Stack, useLocalSearchParams } from "expo-router";
-import { View, Text } from "react-native";
-import { useObject } from "@realm/react";
+import { View, Text, TextInput } from "react-native";
+import { useObject, useRealm } from "@realm/react";
 import { Task } from "../models/Task";
 import { BSON } from "realm";
+import { useState } from "react";
 
 const TaskDetails = () => {
+    // by default with expo router this becomes a navigatable page
+
+    // this is the page of description for each task list item. using [id]
+    // allows this to change based on the variable, like how sites do
+    //twitter.com/[username]
+
+    // this looks for id of page
     const {id} = useLocalSearchParams();
 
     const task = useObject<Task>(Task, new BSON.ObjectId(id as string));
+
+    const [updatedDescription, setUpdatedDescription] = useState(
+        task?.description
+    );
+
+    const realm = useRealm();
+
+    const updateDescription = () => {
+        if (!task) {
+            return;
+        }
+        realm.write(() => {
+            task.description = updatedDescription
+        })
+    }
 
     if (!task) {
         return <Text>not found</Text>
@@ -16,7 +39,12 @@ const TaskDetails = () => {
     return (
         <View style={{padding: 10}}>
             <Stack.Screen options={{title: 'Task Details'}}/>
-            <Text style={{color: 'white', fontSize: 20}}>{task.description}</Text>
+            <TextInput 
+                value={updatedDescription}
+                onChangeText={setUpdatedDescription}
+                onEndEditing={updateDescription}
+                style={{color: 'white', fontSize: 20}}
+            />
         </View>
     )
 }
